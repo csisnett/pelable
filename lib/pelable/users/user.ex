64036ada8f3fc570354bdd3cc15afdfa110bcf5a
Carlos S <sisnett19@gmail.com>
2019-08-25 11:02:41 -0500

    add project_version_user intermediary table

--- lib/pelable/users/user.ex
+++ lib/pelable/users/user.ex
@@ -2,13 +2,16 @@ defmodule Pelable.Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
 
+  alias Pelable.WorkProject.ProjectVersion
+  alias Pelable.Users.User
+
   schema "users" do
     field :username, :string
     field :nickname, :string
     field :fullname, :string
     
+    many_to_many :project_versions, ProjectVersion, join_through: "project_version_user"
     pow_user_fields()
-
     timestamps()
   end
 end
--- lib/pelable/work_project/project_version.ex
+++ lib/pelable/work_project/project_version.ex
@@ -2,6 +2,8 @@ defmodule Pelable.WorkProject.ProjectVersion do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Pelable.Users.User
+
   schema "project_versions" do
     field :description, :string
     field :name, :string
@@ -10,6 +12,7 @@ defmodule Pelable.WorkProject.ProjectVersion do
     field :parent_id, :id
     field :project_id, :id
 
+    many_to_many :users, User, join_through: "project_version_user"
     timestamps()
   end
 
