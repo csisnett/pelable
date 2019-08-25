1e947e235158f65baf80d3b7f25211c18976e341
Carlos S <sisnett19@gmail.com>
2019-08-25 01:35:44 -0500

    create projects schema with gen.html

---
+++ lib/pelable/projects/project.ex
@@ -0,0 +1,19 @@
+defmodule Pelable.Projects.Project do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Pelable.Users.User
+
+  schema "projects" do
+    field :name, :string
+
+    belongs_to :creator, User
+    timestamps()
+  end
+
+  @doc false
+  def changeset(project, attrs) do
+    project
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
---
+++ priv/repo/migrations/20190825060200_create_projects.exs
@@ -0,0 +1,12 @@
+defmodule Pelable.Repo.Migrations.CreateProjects do
+  use Ecto.Migration
+
+  def change do
+    create table(:projects) do
+      add :name, :string
+      add :creator_id, references(:users) 
+      timestamps()
+    end
+
+  end
+end
