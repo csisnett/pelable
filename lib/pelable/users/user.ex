8ff11b4474caf6db888dbe173139609511892ffb
Carlos S <sisnett19@gmail.com>
2019-08-25 15:11:27 -0500

    add work_projects field to user schema

--- lib/pelable/users/user.ex
+++ lib/pelable/users/user.ex
@@ -2,7 +2,7 @@ defmodule Pelable.Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
 
-  alias Pelable.WorkProject.ProjectVersion
+  alias Pelable.WorkProject.{ProjectVersion, WorkProject}
   alias Pelable.Users.User
 
   schema "users" do
@@ -10,6 +10,7 @@ defmodule Pelable.Users.User do
     field :nickname, :string
     field :fullname, :string
     
+    many_to_many :work_projects, WorkProject, join_through: "work_project_user"
     many_to_many :project_bookmarks, ProjectVersion, join_through: "project_version_bookmark"
     pow_user_fields()
     timestamps()
