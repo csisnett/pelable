0fcdee537cd3eca36f4881057d9829369fb0c865
Carlos S <sisnett19@gmail.com>
2019-08-25 23:11:55 -0500

    Add has_many project_versions to Project schema

--- lib/pelable/projects/project.ex
+++ lib/pelable/projects/project.ex
@@ -2,9 +2,11 @@ defmodule Pelable.Projects.Project do
   use Ecto.Schema
   import Ecto.Changeset
   alias Pelable.Users.User
-
+  alias Pelable.WorkProjects.ProjectVersion
+  
   schema "projects" do
     belongs_to :creator, User
+    has_many :project_versions, ProjectVersion
     timestamps()
   end
 
