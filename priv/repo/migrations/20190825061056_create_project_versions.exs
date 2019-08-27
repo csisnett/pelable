cfa7c087d7753bf6e6bf96ac13f479fd217a89c3
Carlos S <sisnett19@gmail.com>
2019-08-27 11:51:02 -0500

    add creator parent and project id indexes prject_V

--- priv/repo/migrations/20190825061056_create_project_versions.exs
+++ priv/repo/migrations/20190825061056_create_project_versions.exs
@@ -7,12 +7,14 @@ defmodule Pelable.Repo.Migrations.CreateProjectVersions do
       add :name, :string
       add :public_status, :string
       add :first?, :boolean
-      add :project_version_id, references(:project_versions)
       add :creator_id, references(:users, on_delete: :nothing)
       add :parent_id, references(:project_versions, on_delete: :nothing)
       add :project_id, references(:projects, on_delete: :nothing)
 
       timestamps()
     end
+    create index(:project_versions, [:creator_id])
+    create index(:project_versions, [:parent_id])
+    create index(:project_versions, [:project_id])
   end
 end
