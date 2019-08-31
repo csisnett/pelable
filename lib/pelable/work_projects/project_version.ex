3b87000e9e37c156d53a615591e41ed3f787354e
Carlos S <sisnett19@gmail.com>
2019-08-31 00:56:11 -0500

    eliminate added_by_id from project_version

--- lib/pelable/work_projects/project_version.ex
+++ lib/pelable/work_projects/project_version.ex
@@ -9,7 +9,6 @@ defmodule Pelable.WorkProjects.ProjectVersion do
     field :first?, :boolean, default: true
     
     has_many :work_projects, WorkProject
-    belongs_to :added_by, User
     belongs_to :parent, ProjectVersion
     timestamps()
   end
@@ -17,8 +16,7 @@ defmodule Pelable.WorkProjects.ProjectVersion do
   @doc false
   def changeset(project_version, attrs) do
     project_version
-    |> cast(attrs, [:first?, :parent_id, :added_by_id])
-    |> validate_required([:added_by_id, :first?])
-    |> foreign_key_constraint(:added_by_id)
+    |> cast(attrs, [:first?, :parent_id])
+    |> validate_required([:first?])
   end
 end
--- priv/repo/migrations/20190825061056_create_project_versions.exs
+++ priv/repo/migrations/20190825061056_create_project_versions.exs
@@ -4,12 +4,10 @@ defmodule Pelable.Repo.Migrations.CreateProjectVersions do
   def change do
     create table(:project_versions) do
       add :first?, :boolean
-      add :added_by_id, references(:users, on_delete: :nothing)
       add :parent_id, references(:project_versions, on_delete: :nothing)
 
       timestamps()
     end
-    create index(:project_versions, [:added_by_id])
     create index(:project_versions, [:parent_id])
   end
 end
