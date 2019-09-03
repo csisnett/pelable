4415e33580a4c07d27161099366381a8609fa5ea
Carlos S <sisnett19@gmail.com>
2019-09-02 23:25:47 -0500

    add iex conf

---
+++ .iex.exs
@@ -0,0 +1,7 @@
+alias Pelable.Projects
+alias Projects.Project
+alias Pelable.Repo
+alias Pelable.WorkProjects
+alias WorkProjects.{WorkProject, WorkProjectUserStory, UserStory, ProjectVersion}
+alias Pelable.Users.User
+alias Pelable.Learn.Goal
