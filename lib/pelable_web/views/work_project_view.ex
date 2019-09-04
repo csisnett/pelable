0a23c08923bfe73035a37d07572da488297d737c
Carlos S <sisnett19@gmail.com>
2019-09-04 10:58:31 -0500

    add support for required/optional user stories

--- lib/pelable_web/views/work_project_view.ex
+++ lib/pelable_web/views/work_project_view.ex
@@ -1,3 +1,26 @@
 defmodule PelableWeb.WorkProjectView do
   use PelableWeb, :view
+
+  def required_or_optional?(:required, %{} = user_story) do
+    user_story.required?
+  end
+
+  def required_or_optional?(:optional, %{} = user_story) do
+    user_story.required? == false
+  end
+
+  def required_user_story?(%{} = user_story) do
+    required_or_optional?(:required, user_story)
+  end
+
+  def optional_user_story?(%{} = user_story) do
+    required_or_optional?(:optional, user_story)
+  end
+
+  #[] -> boolean
+  # Returns false if there isn't a single user story with required? = false
+  def optional_user_stories?(user_stories) when is_list(user_stories) do
+    Enum.reduce(user_stories, false, fn u, acc -> (optional_user_story?(u)) || acc end)
+  end
+
 end
