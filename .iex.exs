57504a42c626c88c444ff51daab436439a7d8f14
Carlos S <sisnett19@gmail.com>
2019-09-07 10:24:22 -0500

    add generate_uuid fn to chatroom

--- .iex.exs
+++ .iex.exs
@@ -5,3 +5,4 @@ alias Pelable.WorkProjects
 alias WorkProjects.{WorkProject, WorkProjectUserStory, UserStory, ProjectVersion}
 alias Pelable.Users.User
 alias Pelable.Learn.Goal
+Pelable.Chat.{Chatroom, Message}
--- lib/pelable/chat/chatroom.ex
+++ lib/pelable/chat/chatroom.ex
@@ -14,11 +14,23 @@ defmodule Pelable.Chat.Chatroom do
     timestamps()
   end
 
+  def generate_random_uuid(length) do
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
+  end
+
+  def generate_uuid(changeset) do
+    case get_field(changeset, :uuid) do
+      nil -> changeset |> put_change(:uuid, generate_random_uuid(12))
+      _ -> changeset
+    end
+  end
+
   @doc false
   def changeset(chatroom, attrs) do
     chatroom
     |> cast(attrs, [:uuid, :description, :name, :topic, :creator_id])
     |> validate_required([:uuid, :description, :name, :topic, :creator_id])
+    |> generate_uuid
     |> unique_constraint(:uuid)
   end
 end
