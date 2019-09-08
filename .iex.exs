a19a46cb4e4f79c5cdd700272a64faf91ab1047d
Carlos S <sisnett19@gmail.com>
2019-09-08 11:59:02 -0500

    add Learn context to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -4,6 +4,7 @@ alias Pelable.Repo
 alias Pelable.WorkProjects
 alias WorkProjects.{WorkProject, WorkProjectUserStory, UserStory, ProjectVersion}
 alias Pelable.Users.User
-alias Pelable.Learn.Goal
+alias Pelable.Learn
+alias Learn.Goal
 alias Pelable.Chat
 alias Pelable.Chat.{Chatroom, Message}
