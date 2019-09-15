27837df4aae6fbd55b538374b998b29fa553a13a
Carlos Sisnett <carlos@pelable.com>
2019-09-15 12:15:34 -0500

    modify user_stories migration

---
+++ priv/repo/migrations/20190915165753_modify_user_stories_table.exs
@@ -0,0 +1,13 @@
+defmodule Pelable.Repo.Migrations.ModifyUserStoriesTable do
+  use Ecto.Migration
+
+  def change do
+    alter table("user_stories") do
+      add :work_project_id, references(:work_projects)
+      add :status, :string, null: false
+      add :required?, :boolean
+  end
+  drop index("user_stories", [:title])
+  create index("user_stories", [:title])
+end
+end
