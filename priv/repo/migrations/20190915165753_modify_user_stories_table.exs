394be3455ff32ad3b42e22a83756394191060eb8
Carlos Sisnett <carlos@pelable.com>
2019-09-17 05:05:32 -0500

    put modifications of user stories in one migration

--- priv/repo/migrations/20190915165753_modify_user_stories_table.exs
+++ priv/repo/migrations/20190915165753_modify_user_stories_table.exs
@@ -6,8 +6,10 @@ defmodule Pelable.Repo.Migrations.ModifyUserStoriesTable do
       add :work_project_id, references(:work_projects)
       add :status, :string, null: false
       add :required?, :boolean
+      add :uuid, :uuid, null: false
   end
   drop index(:user_stories, [:title])
   create index(:user_stories, [:title])
+  create unique_index(:user_stories, [:uuid])
 end
 end
