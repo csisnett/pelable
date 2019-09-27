654d38017c7cc19ac639f6cc957c2d3dafb3235a
Carlos Sisnett <carlos@pelable.com>
2019-09-27 01:32:45 -0500

    render new messages when rendering template

--- lib/pelable_web/views/chatroom_view.ex
+++ lib/pelable_web/views/chatroom_view.ex
@@ -1,3 +1,8 @@
 defmodule PelableWeb.ChatroomView do
+  alias Pelable.Chat
   use PelableWeb, :view
+
+  def seen_last_message?(user, chatroom) do
+    Chat.seen_last_message?(user, chatroom)
+  end
 end
