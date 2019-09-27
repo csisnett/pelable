f335079eb42b13f18ec7a5da0110f65027b95f24
Carlos Sisnett <carlos@pelable.com>
2019-09-27 04:38:22 -0500

    render html in chat messages safely (escaped)

--- lib/pelable_web/views/chatroom_view.ex
+++ lib/pelable_web/views/chatroom_view.ex
@@ -1,8 +1,17 @@
 defmodule PelableWeb.ChatroomView do
   alias Pelable.Chat
   use PelableWeb, :view
+  alias Pelable.Chat.Message
 
   def seen_last_message?(user, chatroom) do
     Chat.seen_last_message?(user, chatroom)
+  end
+
+  def render_message(%Message{} = message) do
+    {:safe, datetime} = content_tag(:datetime, message.inserted_at)
+    {:safe, username} = content_tag(:b, " #{message.sender.username}" <> ": ")
+    {:safe, body} = html_escape(message.body)
+    {:safe, [60, "p", [], 62, datetime, username, body, 60, 47, "p", 62]}
+    
   end
 end
