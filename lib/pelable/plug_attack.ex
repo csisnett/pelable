4e0f274972c30e1a92c772e58dbe392c0a9cfd12
Carlos Sisnett <carlos@pelable.com>
2019-09-29 02:26:13 -0500

    change order to block/throttle/allow

--- lib/pelable/plug_attack.ex
+++ lib/pelable/plug_attack.ex
@@ -2,11 +2,6 @@ defmodule Pelable.PlugAttack do
     use PlugAttack
     require Logger
 
-    rule "allow local", conn do
-        Logger.warn("#{inspect conn.remote_ip}")
-        allow conn.remote_ip == {127, 0, 0, 1}
-    end
-
     rule "block annoying bot", conn do
         case conn.remote_ip do
             {0, 0, 0, 0, 0, 65535, 2660, x} -> block true
@@ -17,5 +12,10 @@ defmodule Pelable.PlugAttack do
         throttle conn.remote_ip,
           period: 60_000, limit: 10,
           storage: {PlugAttack.Storage.Ets, Pelable.PlugAttack.Storage}
+    end
+
+    rule "allow local", conn do
+        Logger.warn("#{inspect conn.remote_ip}")
+        allow conn.remote_ip == {127, 0, 0, 1}
     end
 end
