991f898bb43758d979cada409173291cbf534e4c
Carlos Sisnett <carlos@pelable.com>
2019-09-29 01:38:54 -0500

    change myapp to Pelable in storage setting

--- lib/pelable/plug_attack.ex
+++ lib/pelable/plug_attack.ex
@@ -1,13 +1,9 @@
 defmodule Pelable.PlugAttack do
     use PlugAttack
 
-    rule "allow local", conn do
-        allow conn.remote_ip == {127,0,0,1}
-    end
-
     rule "throttle by ip", conn do
         throttle conn.remote_ip,
           period: 60_000, limit: 10,
-          storage: {PlugAttack.Storage.Ets, MyApp.PlugAttack.Storage}
+          storage: {PlugAttack.Storage.Ets, Pelable.PlugAttack.Storage}
     end
 end
