f80a565d23860c38b1ff5c028ba2309fb68d96fb
Carlos Sisnett <carlos@pelable.com>
2019-09-29 04:02:10 -0500

    the block bot rule is back

--- lib/pelable/plug_attack.ex
+++ lib/pelable/plug_attack.ex
@@ -2,11 +2,18 @@ defmodule Pelable.PlugAttack do
     use PlugAttack
     require Logger
 
+    rule "block bot", conn do
+        case conn.remote_ip do
+            {0, 0, 0, 0, 0, 65535, 2660, x} -> block true
+        end
+        
+    end
+
     rule "allow local", conn do
         Logger.warn("#{inspect conn.remote_ip}")
         allow conn.remote_ip == {127, 0, 0, 1}
     end
-    
+
     rule "throttle by ip", conn do
         throttle conn.remote_ip,
           period: 60_000, limit: 10,
