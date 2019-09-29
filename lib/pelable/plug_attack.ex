e24b4969f94a965682ffb3c6175b4b5453504d74
Carlos Sisnett <carlos@pelable.com>
2019-09-29 03:16:36 -0500

    move allow local rule to the front to see ips

--- lib/pelable/plug_attack.ex
+++ lib/pelable/plug_attack.ex
@@ -2,14 +2,16 @@ defmodule Pelable.PlugAttack do
     use PlugAttack
     require Logger
 
+    rule "allow local", conn do
+        Logger.warn("#{inspect conn.remote_ip}")
+        allow conn.remote_ip == {127, 0, 0, 1}
+    end
+    
     rule "throttle by ip", conn do
         throttle conn.remote_ip,
           period: 60_000, limit: 10,
           storage: {PlugAttack.Storage.Ets, Pelable.PlugAttack.Storage}
     end
 
-    rule "allow local", conn do
-        Logger.warn("#{inspect conn.remote_ip}")
-        allow conn.remote_ip == {127, 0, 0, 1}
-    end
+    
 end
