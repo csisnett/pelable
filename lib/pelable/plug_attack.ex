6f3b31212647a55f70e5cca81b027464eaf39ec4
Carlos Sisnett <carlos@pelable.com>
2019-10-03 14:51:16 -0500

    delete plug attack rules for render bug

--- lib/pelable/plug_attack.ex
+++ lib/pelable/plug_attack.ex
@@ -2,19 +2,6 @@ defmodule Pelable.PlugAttack do
     use PlugAttack
     require Logger
 
-    rule "allow health check", conn do
-        Logger.warn("#{inspect conn.remote_ip}")
-        allow conn.request_path == "/check39432"
-    end
-
-    rule "block bot", conn do
-        case conn.remote_ip do
-            {0, 0, 0, 0, 0, 65535, 2660, _x} -> block true
-            _anything_else -> block false
-        end
-        
-    end
-
     rule "allow local", conn do
         Logger.warn("#{inspect conn.remote_ip}")
         allow conn.remote_ip == {127, 0, 0, 1}
