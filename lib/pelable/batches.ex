31cf3479d1b877ea8e255d840a726ee56773e6f6
Carlos Sisnett <carlos@pelable.com>
2019-10-13 02:47:21 -0500

    create fns to create team chat and direct messages

--- lib/pelable/batches.ex
+++ lib/pelable/batches.ex
@@ -29,14 +29,45 @@ defmodule Pelable.Batches do
 
     #To make Teams
 
+    def convert_usernames(users) when is_list(users) do
+        Enum.map(users, fn username -> Repo.get_by(User, username: username) end)
+    end
+
     # [username, ..., username], String -> %Chatroom{"type" => "private group", ""}
     # Takes a list of usernames and join the respective users to a new chatroom.
     def create_team_chat(user_list, team_name) when is_list(user_list) do
-        user_list = Enum.map(user_list, fn username -> Repo.get_by(User, username: username) end)
+        user_list = convert_usernames(user_list)
         chatroom = Chat.create_chatroom_assoc(%{"type" => "private group", "creator_id" => 1, "name" => team_name}) |> Repo.preload([:participants])
         chatroom_changeset = Ecto.Changeset.change(chatroom)
         chatroom_participants_changeset = chatroom_changeset |> Ecto.Changeset.put_assoc(:participants, user_list)
         Repo.update!(chatroom_participants_changeset)
+    end
+
+
+    def create_directs_for_user(%User{} = user, user_list) when is_list(user_list) do
+        case length(user_list) do
+            0 -> :ok
+            x ->
+                [first | rest] = user_list
+                two_users = [user, first]
+                chatroom = Chat.create_chatroom_assoc(%{"type" => "private conversation", "creator_id" => 1, "name" => "Direct Message"}) |> Repo.preload([:participants])
+                chatroom_changeset = Ecto.Changeset.change(chatroom)
+                chatroom_participants_changeset = chatroom_changeset |> Ecto.Changeset.put_assoc(:participants, two_users)
+                Repo.update!(chatroom_participants_changeset)
+                create_directs_for_user(user, rest)
+
+        end
+    end
+
+    def create_direct_chats(user_list) when is_list(user_list) do
+        case length(user_list) do
+           1 -> :ok
+           x -> 
+        
+        [first | rest] = user_list
+        create_directs_for_user(first, rest)
+        create_direct_chats(rest)
+        end
     end
 
     def dead_users do
