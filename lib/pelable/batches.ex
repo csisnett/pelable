07dca4290e1b42f2ae53fe497a146cd2615f4dd5
Carlos Sisnett <carlos@pelable.com>
2019-10-13 01:44:55 -0500

    create_team_chat in batches context

--- lib/pelable/batches.ex
+++ lib/pelable/batches.ex
@@ -9,6 +9,8 @@ defmodule Pelable.Batches do
     alias Pelable.Chat
     alias PelableWeb.PowMailer
 
+    #To Match users
+
     def get_user_from_message(%Message{} = message) do
         Learn.get_user!(message.sender_id)
     end
@@ -23,6 +25,18 @@ defmodule Pelable.Batches do
         user = Repo.get_by(User, username: username)
         query = from m in Message, where: m.sender_id == ^user.id and m.chatroom_id == ^chatroom_id
         Repo.all(query) 
+    end
+
+    #To make Teams
+
+    # [username, ..., username], String -> %Chatroom{"type" => "private group", ""}
+    # Takes a list of usernames and join the respective users to a new chatroom.
+    def create_team_chat(user_list, team_name) when is_list(user_list) do
+        user_list = Enum.map(user_list, fn username -> Repo.get_by(User, username: username) end)
+        chatroom = Chat.create_chatroom_assoc(%{"type" => "private group", "creator_id" => 1, "name" => team_name}) |> Repo.preload([:participants])
+        chatroom_changeset = Ecto.Changeset.change(chatroom)
+        chatroom_participants_changeset = chatroom_changeset |> Ecto.Changeset.put_assoc(:participants, user_list)
+        Repo.update!(chatroom_participants_changeset)
     end
 
     def dead_users do
