7ee055155fc07fcf44b774658ca3c6c6ca26151b
Carlos Sisnett <carlos@pelable.com>
2019-10-18 21:14:07 -0500

    add one signal script to header

--- lib/pelable_web/views/layout_view.ex
+++ lib/pelable_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule PelableWeb.LayoutView do
   use PelableWeb, :view
+
+  def show_web_push?(conn) do
+    case conn.request_path do
+      "/chat/" <> uuid -> true
+      other -> false 
+    end
+  end
 end
