b34b3c5b3808a1d76dbd3a6efe7ed8130aab1967
Carlos Sisnett <carlos@pelable.com>
2019-10-29 16:56:02 -0500

    generates expiration date for team chatrooms

--- lib/pelable/chat/chatroom.ex
+++ lib/pelable/chat/chatroom.ex
@@ -11,7 +11,7 @@ defmodule Pelable.Chat.Chatroom do
     field :description, :string
     field :type, :string, default: "public"
     field :expires_at, :utc_datetime
-    field :expired?, :boolean
+    field :expired?, :boolean, default: false
 
     field :uuid, :string
     belongs_to :creator, User
@@ -22,15 +22,31 @@ defmodule Pelable.Chat.Chatroom do
     timestamps()
   end
 
-  def generate_random_uuid(length) do
+  def new_random_uuid(length) do
     :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
   end
 
   def generate_uuid(changeset) do
     case get_field(changeset, :uuid) do
-      nil -> changeset |> put_change(:uuid, generate_random_uuid(12))
+      nil -> changeset |> put_change(:uuid, new_random_uuid(12))
       _ -> changeset
     end
+  end
+
+  #Expires 48 hours after its creation
+  def new_expiration_date do
+    DateTime.utc_now |> DateTime.add(86400 * 2) |> DateTime.truncate(:second)
+  end
+
+  def generate_expiration_date(changeset) do
+    case get_field(changeset, :type) do
+    "private group team" ->
+      case get_field(changeset, :expires_at) do
+      nil -> changeset |> put_change(:expires_at, new_expiration_date)
+        _ -> changeset
+      end
+      _another_type -> changeset
+  end
   end
 
   @doc false
@@ -39,6 +55,7 @@ defmodule Pelable.Chat.Chatroom do
     |> cast(attrs, [:uuid, :description, :name, :creator_id, :type])
     |> validate_required([:name, :creator_id, :type])
     |> generate_uuid
+    |> generate_expiration_date
     |> unique_constraint(:uuid)
     |> foreign_key_constraint(:creator_id)
   end
