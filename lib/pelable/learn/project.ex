a5afc47de2cf74e026ad6d5db02adc909f452529
Carlos Sisnett <carlos@pelable.com>
2020-01-21 16:38:07 -0500

    add slug to project

--- lib/pelable/learn/project.ex
+++ lib/pelable/learn/project.ex
@@ -2,13 +2,18 @@ defmodule Pelable.Learn.Project do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Pelable.Users.User
+  alias Pelable.Learn.{NameSlug}
   alias Pelable.Learn.Workspace
 
   schema "projects" do
-    field :description, :string
     field :name, :string
+    field :description, :string
+    field :slug, NameSlug.Type
     field :uuid, Ecto.ShortUUID, autogenerate: true
 
+
+    belongs_to :creator, User
     belongs_to :workspace, Workspace
 
     timestamps()
@@ -17,8 +22,9 @@ defmodule Pelable.Learn.Project do
   @doc false
   def changeset(project, attrs) do
     project
-    |> cast(attrs, [:name, :description, :workspace_id])
-    |> validate_required([:name, :workspace_id])
+    |> cast(attrs, [:name, :description, :workspace_id, :creator_id])
+    |> validate_required([:name, :workspace_id, :creator_id])
     |> unique_constraint(:uuid)
+    |> NameSlug.maybe_generate_slug
   end
 end
