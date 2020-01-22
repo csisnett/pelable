b0d50aa4066b57b30f78578eaa5d9962b5aadb70
Carlos Sisnett <carlos@pelable.com>
2020-01-21 22:44:52 -0500

    delete Goal alias from .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -2,7 +2,7 @@ alias Pelable.Projects
 alias Pelable.Repo
 alias Pelable.Users.User
 alias Pelable.Learn
-alias Pelable.Learn.{Goal, Workspace, WorkspaceMember, Project}
+alias Pelable.Learn.{Workspace, WorkspaceMember, Project, Section}
 alias Pelable.Chat
 alias Pelable.Batches
 alias Pelable.Chat.{Chatroom, Message, Participant, Invitation, Mention}
