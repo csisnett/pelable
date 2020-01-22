d861cabe79d9eb3f98556a91f8ce66e3f821cda7
Carlos Sisnett <carlos@pelable.com>
2020-01-22 12:41:05 -0500

    add Post alias to iex.exs

--- .iex.exs
+++ .iex.exs
@@ -2,7 +2,7 @@ alias Pelable.Projects
 alias Pelable.Repo
 alias Pelable.Users.User
 alias Pelable.Learn
-alias Pelable.Learn.{Workspace, WorkspaceMember, Project, Section}
+alias Pelable.Learn.{Workspace, WorkspaceMember, Project, Section, Post}
 alias Pelable.Chat
 alias Pelable.Batches
 alias Pelable.Chat.{Chatroom, Message, Participant, Invitation, Mention}
