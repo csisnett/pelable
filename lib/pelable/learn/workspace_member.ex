741411596b591a24f1427723f32b50af13157c1d
Carlos Sisnett <carlos@pelable.com>
2020-09-12 12:03:02 -0500

    add :personal? to workspace_member schema

--- lib/pelable/learn/workspace_member.ex
+++ lib/pelable/learn/workspace_member.ex
@@ -7,7 +7,8 @@ defmodule Pelable.Learn.WorkspaceMember do
 
   schema "workspace_member" do
     field :withdrawal, :naive_datetime
-    
+    field :personal?, :boolean
+
     belongs_to :user, User
     belongs_to :workspace, Workspace
     timestamps()
@@ -16,8 +17,8 @@ defmodule Pelable.Learn.WorkspaceMember do
   @doc false
   def changeset(workspace_member, attrs) do
     workspace_member
-    |> cast(attrs, [:user_id, :workspace_id, :withdrawal])
-    |> validate_required([:user_id, :workspace_id])
+    |> cast(attrs, [:user_id, :workspace_id, :withdrawal, :personal?])
+    |> validate_required([:user_id, :workspace_id, :personal?])
     |> foreign_key_constraint(:user_id)
     |> foreign_key_constraint(:workspace_id)
   end
