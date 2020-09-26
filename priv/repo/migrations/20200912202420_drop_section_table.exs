216573e70d2d1fab68d462356d489de680c4f2e4
Carlos Sisnett <carlos@pelable.com>
2020-09-26 16:14:20 -0500

    delete removal of f_key constraint in threads

--- priv/repo/migrations/20200912202420_drop_section_table.exs
+++ priv/repo/migrations/20200912202420_drop_section_table.exs
@@ -2,7 +2,6 @@ defmodule Pelable.Repo.Migrations.DropSectionTable do
   use Ecto.Migration
 
   def change do
-    drop (constraint(:threads, :threads_section_id_fkey))
     drop table("sections")
   end
 end
