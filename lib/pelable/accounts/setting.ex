b9d770eb314cb76ab73de6b269fabff280ab9829
Carlos Sisnett <carlos@pelable.com>
2020-09-28 01:28:09 -0500

    create function to validate setting keys

--- lib/pelable/accounts/setting.ex
+++ lib/pelable/accounts/setting.ex
@@ -3,19 +3,31 @@ defmodule Pelable.Accounts.Setting do
   import Ecto.Changeset
   alias Pelable.Users.User
 
+  @valid_setting_keys ["timezone"]
+
   schema "settings" do
-    field :key, :string
+    field :setting_key, :string
     field :value, :string
 
     belongs_to :user, User
     timestamps()
   end
 
+  def validate_setting_key(changeset) do
+    {_, setting_key} = fetch_field(changeset, :setting_key)
+    case Enum.member?(@valid_setting_keys, setting_key) do
+      true -> changeset
+      false -> add_error(changeset, :setting_key, "Not a valid setting key")
+    end
+  end
+
+
   @doc false
-  def changeset(settings, attrs) do
-    settings
-    |> cast(attrs, [:key, :value, :user_id])
-    |> validate_required([:key, :value, :user_id])
+  def changeset(setting, attrs) do
+    setting
+    |> cast(attrs, [:setting_key, :value, :user_id])
+    |> validate_required([:setting_key, :value, :user_id])
+    |> validate_setting_key
     |> foreign_key_constraint(:user_id)
   end
 end
