93b1f25c817933a0ec00b5e80c38d93f2695d410
Carlos Sisnett <carlos@pelable.com>
2020-09-28 01:27:51 -0500

    modify create_setting migration 'setting_key'

--- priv/repo/migrations/20200927180624_create_settings.exs
+++ priv/repo/migrations/20200927180624_create_settings.exs
@@ -3,13 +3,13 @@ defmodule Pelable.Repo.Migrations.CreateSettings do
 
   def change do
     create table(:settings) do
-      add :key, :string
-      add :value, :string
+      add :setting_key, :string, null: false
+      add :value, :string, null: false
 
       add :user_id, references(:users, on_delete: :delete_all), null: false
       timestamps()
     end
     create index(:settings, [:user_id])
-    create unique_index(:settings, [:user_id, :key])
+    create unique_index(:settings, [:user_id, :setting_key])
   end
 end
