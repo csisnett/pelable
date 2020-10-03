c54ab44a8ea7e1bbfbd68114521a0631cf008e62
Carlos Sisnett <carlos@pelable.com>
2020-10-03 02:27:34 -0500

    add validator for timefrequency in Habit changeset

--- lib/pelable/habits/habit.ex
+++ lib/pelable/habits/habit.ex
@@ -2,6 +2,7 @@ defmodule Pelable.Habits.Habit do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @time_frequencies ["hourly", "daily", "weekly", "monthly", "quarterly"]
   schema "habits" do
 
     field :name, :string
@@ -13,11 +14,20 @@ defmodule Pelable.Habits.Habit do
     timestamps()
   end
 
+  def validate_time_frequency(changeset) do
+    {_, time_frequency} = fetch_field(changeset, :time_frequency)
+    case Enum.member?(@time_frequencies, time_frequency) do
+      true -> changeset
+      false -> add_error(changeset, :time_frequency, "Not a valid time frequency")
+    end
+  end
+
   @doc false
   def changeset(habit, attrs) do
     habit
     |> cast(attrs, [:name, :time_frequency, :archived?, :user_id])
     |> validate_required([:name, :time_frequency, :user_id])
+    |> validate_time_frequency
     |> foreign_key_constraint(:user_id)
   end
 end
