c46b25b637b6f635b9e8df0b7bd8540b6853718f
Carlos Sisnett <carlos@pelable.com>
2020-10-06 15:34:33 -0500

    create rewards migration

---
+++ priv/repo/migrations/20201006192816_create_rewards.exs
@@ -0,0 +1,17 @@
+defmodule Pelable.Repo.Migrations.CreateRewards do
+  use Ecto.Migration
+
+  def change do
+    create table(:rewards) do
+      add :name, :string, null: false
+      add :description, :string
+      add :uuid, :uuid, null: false
+      add :creator_id, references(:users, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+
+    create index(:rewards, [:creator_id])
+    create unique_index(:rewards, [:uuid])
+  end
+end
