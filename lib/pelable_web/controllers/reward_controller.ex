57837dc359ba579a92cf0c2c92f5976ac9e44926
Carlos Sisnett <carlos@pelable.com>
2020-10-13 21:14:54 -0500

    update reward templates &controller to :slug/:uuid

--- lib/pelable_web/controllers/reward_controller.ex
+++ lib/pelable_web/controllers/reward_controller.ex
@@ -15,44 +15,45 @@ defmodule PelableWeb.RewardController do
   end
 
   def create(conn, %{"reward" => reward_params}) do
-    case Habits.create_reward(reward_params) do
+    user = conn.assigns.current_user
+    case Habits.create_reward(reward_params, user) do
       {:ok, reward} ->
         conn
         |> put_flash(:info, "Reward created successfully.")
-        |> redirect(to: Routes.reward_path(conn, :show, reward))
+        |> redirect(to: Routes.reward_path(conn, :show, reward.slug, reward.uuid))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    reward = Habits.get_reward!(id)
+  def show(conn, %{"uuid" => uuid}) do
+    reward = Habits.get_reward_by_uuid(uuid)
     render(conn, "show.html", reward: reward)
   end
 
-  def edit(conn, %{"id" => id}) do
-    reward = Habits.get_reward!(id)
+  def edit(conn, %{"uuid" => uuid}) do
+    reward = Habits.get_reward_by_uuid(uuid)
     changeset = Habits.change_reward(reward)
     render(conn, "edit.html", reward: reward, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "reward" => reward_params}) do
-    reward = Habits.get_reward!(id)
+  def update(conn, %{"uuid" => uuid, "reward" => reward_params}) do
+    reward = Habits.get_reward_by_uuid(uuid)
 
     case Habits.update_reward(reward, reward_params) do
       {:ok, reward} ->
         conn
         |> put_flash(:info, "Reward updated successfully.")
-        |> redirect(to: Routes.reward_path(conn, :show, reward))
+        |> redirect(to: Routes.reward_path(conn, :show, reward.slug, reward.uuid))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", reward: reward, changeset: changeset)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    reward = Habits.get_reward!(id)
+  def delete(conn, %{"uuid" => uuid}) do
+    reward = Habits.get_reward_by_uuid(uuid)
     {:ok, _reward} = Habits.delete_reward(reward)
 
     conn
